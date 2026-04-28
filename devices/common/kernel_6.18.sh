da84b5f7ec8ce5bdb1383a344ac61525df307b94
gongzi miao <miaogongzi0227@gmail.com>
2026-04-28 10:59:51 +0800

    Update kernel_6.18.sh

--- devices/common/kernel_6.18.sh
+++ devices/common/kernel_6.18.sh
@@ -11,21 +11,6 @@ cp -rf --parents target/linux package/boot package/devel package/firmware packag
 
 cd -
 
-#nat46: fix reproducible-build failure and use latest git
-#wget -N https://github.com/graysky2/openwrt/commit/e52d04b65d1942f581533cb2054e74f4ff5bd70b.patch -P devices/common/patches/
-
-
-#nat46: fix reproducible-build failure and use latest git
-#wget -N https://github.com/graysky2/openwrt/commit/e52d04b65d1942f581533cb2054e74f4ff5bd70b.patch -P devices/common/patches/
-
-#kernel: 6.18: backport mxl862xx driver for Linxu 6.18 #22668
-#wget -N https://github.com/dangowrt/openwrt/commit/ae28585b65a80600b5e4502aba594f2cdcc1d52e.patch -P devices/common/patches/
-
-#wget -N https://raw.githubusercontent.com/immortalwrt-collections/lean-lede/refs/heads/lede/target/linux/x86/patches-6.18/996-intel-igc-i225-i226-disable-eee.patch -P target/linux/x86/patches-6.18/
-
-
-
-
 
 cd feeds/packages
 rm -rf net/xtables-addons net/jool kernel/v4l2loopback libs/libpfring libs/libmariadb
@@ -37,8 +22,6 @@ cd ../../
 #cd feeds/miaogongzi
 #rm -rf fibocom_QMI_WWAN rkp-ipid
 #cd ../../
-
-
 
 cd package
 rm -rf devel/kselftests-bpf  kernel/mt76 kernel/ath10k-ct 
